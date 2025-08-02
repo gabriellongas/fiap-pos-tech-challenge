8ebdefe1905f8eb8dec85c006c135dd2b06fae85
Pedro Luperini <pedrolpiza@yahoo.com.br>
2025-08-02 08:09:15 -0300

    feat(game): add game entity and its migration

--- src/FIAP.CloudGames.Infrastructure/Context/AppDbContext.cs
+++ src/FIAP.CloudGames.Infrastructure/Context/AppDbContext.cs
@@ -12,6 +12,7 @@ public class AppDbContext: DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     DbSet<User> Users { get; set; }
+    DbSet<Game> Games { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- src/FIAP.CloudGames.Infrastructure/Migrations/20250802110424_AddGameEntity.Designer.cs
+++ src/FIAP.CloudGames.Infrastructure/Migrations/20250802110424_AddGameEntity.Designer.cs
@@ -4,6 +4,7 @@ using FIAP.CloudGames.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FIAP.CloudGames.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250802110424_AddGameEntity")]
+    partial class AddGameEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
