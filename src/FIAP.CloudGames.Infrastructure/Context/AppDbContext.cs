9095e30b682aa249c429ad5f25a6689b214d7eb3
Pedro Luperini <pedrolpiza@yahoo.com.br>
2025-08-08 20:13:09 -0300

    feat(user): user seeder

--- src/FIAP.CloudGames.Infrastructure/Context/AppDbContext.cs
+++ src/FIAP.CloudGames.Infrastructure/Context/AppDbContext.cs
@@ -11,8 +11,8 @@ public class AppDbContext: DbContext
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    DbSet<User> Users { get; set; }
-    DbSet<Game> Games { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Game> Games { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
